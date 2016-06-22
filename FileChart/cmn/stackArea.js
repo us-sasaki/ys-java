325865866c33db5d2cf69a2cf56c8377b041fd46
us-sasaki <syusuke@pony.ocn.ne.jp>
2016-06-22 21:57:03 +0900

    clean src

--- FileChart/cmn/stackArea.js
+++ FileChart/cmn/stackArea.js
@@ -6,8 +6,8 @@ d3.json(filename, function(myData) {
 nv.addGraph(function() {
     var chart = nv.models.stackedAreaChart()
                   .margin({right: 100})
-                  .x(function(d) { return d[0] })   //We can modify the data accessor functions...
-                  .y(function(d) { return d[1] })   //...in case your data is formatted differently.
+//                  .x(function(d) { return d[0] })   //We can modify the data accessor functions...
+//                  .y(function(d) { return d[1] })   //...in case your data is formatted differently.
                   .useInteractiveGuideline(true)    //Tooltips which show all data points. Very nice!
                   .rightAlignYAxis(true)      //Let's move the y-axis to the right side.
                   .transitionDuration(500)
