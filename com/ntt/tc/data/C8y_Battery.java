ddb4882f74db3c2a155689ea0e671a1ff93cdd02
us-sasaki <syusuke@pony.ocn.ne.jp>
2016-12-26 07:29:27 +0900

    battery modified

--- com/ntt/tc/data/C8y_Battery.java
+++ com/ntt/tc/data/C8y_Battery.java
@@ -3,7 +3,7 @@ package com.ntt.tc.data;
 import abdom.data.json.object.JData;
 import com.ntt.tc.data.sensor.Value;
 
-public class C8y_Battery extends JData {
+public class C8y_Battery extends C8yData {
 	public C8y_Battery() {
 		super();
 	}
