d4e60afd5432e0b55b193a2d1429296eb1456ac9
us-sasaki <syusuke@pony.ocn.ne.jp>
2018-03-29 21:59:03 +0900

    PCA

--- sample/20180311_PCA/PCA.py
+++ sample/20180311_PCA/PCA.py
@@ -27,3 +27,29 @@ cov_mat = np.cov(X_train_std.T)
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 
 print('\nEigenvalues\n%s' % eigen_vals)
+
+# (固有値、固有ベクトル)のタプルのリストを作成
+eigen_pairs = [(np.abs(eigen_vals[i]),eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+
+# (固有値、固有ベクトル)のタプルを大きいものから順に並べ替え
+eigen_pairs.sort(reverse=True)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:,np.newaxis]))
+print('Matrix W:\n', w)
+
+print(X_train_std[0].dot(w))
+
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+# 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを生成してプロット
+import matplotlib.pyplot as plt
+for l, c, m in zip(np.unique(y_train), colors, markers):
+	plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1],
+	c=c, label=l, marker=m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
