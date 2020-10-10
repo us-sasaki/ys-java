e3755af92dfd4f79957d19e96edf5d91eca3b4ce
Yusuke Sasaki <syusuke@pony.ocn.ne.jp>
2020-10-10 18:25:21 +0900

    コラッツ予想実験

---
+++ sample/20201010_Collatz予想/Collatz.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Collatz 予想が正しいか、有限の数までで確認する。
+ * 無限ループ、または例外が出なければ指定した数までは正しい。
+ */
+public class Collatz {
+	Set<Long> solved;
+	
+	private void solve(long number) {
+		if (number == 1) return;
+		if (number > Long.MAX_VALUE/3)
+			throw new RuntimeException("value exceeded: "+ number);
+		if (solved.contains(number)) return;
+		solved.add(number);
+		if (number%2 == 0) solve(number/2);
+		else {
+			solve(number*3+1);
+		}
+	}
+	
+	public Collatz(int arg) {
+		solved = new HashSet<>();
+		for (int i = 2; i < arg; i++) {
+			solve(i);
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		Collatz c = new Collatz(2000000);
+		long max = 0;
+		for (long v : c.solved) {
+			max = Math.max(max, v);
+		}
+		System.out.println(max);
+	}
+}
