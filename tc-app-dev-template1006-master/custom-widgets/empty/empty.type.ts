895201ed8bd8f17bc5f8648a94890b7f477ed7d8
us-sasaki <syusuke@pony.ocn.ne.jp>
2023-02-15 14:50:40 +0900

    feat: rename directory

--- tc-app-dev-template1006-master/custom-widgets/empty/empty.type.ts
+++
@@ -1,4 +0,0 @@
-export interface EmptyType {
-    // This is a sample of external interface,
-    // so it has no members.
-}
--- tc-app-dev-template1006-master/custom-widgets/empty/empty/empty-config.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'empty-config',
-  templateUrl: './empty-config.component.html'
-})
-export class EmptyConfig {
-  @Input() config: any = {};
-}
