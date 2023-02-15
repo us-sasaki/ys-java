510068a99a7586fa68b3a0162b6d064033de084b
us-sasaki <syusuke@pony.ocn.ne.jp>
2023-02-15 14:51:50 +0900

    feat: exec c8ycli to rewrite websdk files

--- cockpit/app.module.ts
+++ cockpit/app.module.ts
@@ -1,14 +1,7 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule as NgRouterModule } from '@angular/router';
 import { UpgradeModule as NgUpgradeModule } from '@angular/upgrade/static';
-import { HttpClientModule } from '@angular/common/http';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { TimepickerModule } from 'ngx-bootstrap/timepicker';
-
 import { CoreModule, RouterModule } from '@c8y/ngx-components';
 import {
   DashboardUpgradeModule,
@@ -16,54 +9,39 @@ import {
   HybridAppModule,
   UPGRADE_ROUTES
 } from '@c8y/ngx-components/upgrade';
-import { AssetsNavigatorModule } from '@c8y/ngx-components/assets-navigator';
-import { CockpitDashboardModule } from '@c8y/ngx-components/context-dashboard';
+import { SubAssetsModule } from '@c8y/ngx-components/sub-assets';
+import { ChildDevicesModule } from '@c8y/ngx-components/child-devices';
+import {
+  CockpitDashboardModule,
+  ReportDashboardModule
+} from '@c8y/ngx-components/context-dashboard';
 import { ReportsModule } from '@c8y/ngx-components/reports';
 import { SensorPhoneModule } from '@c8y/ngx-components/sensor-phone';
 import { BinaryFileDownloadModule } from '@c8y/ngx-components/binary-file-download';
-
-import { EmptyModule } from './custom-widgets/empty/empty.module';
-import { ThingsCloudModule } from './custom-widgets/common/thingsCloud.module';
-
-import { C8yService } from './custom-widgets/common/c8y/c8y.service';
-import { C8yCommonService } from './custom-widgets/common/c8y/c8ycommon.service';
+import { SearchModule } from '@c8y/ngx-components/search';
+import { AssetsNavigatorModule } from '@c8y/ngx-components/assets-navigator';
+import { CockpitConfigModule } from '@c8y/ngx-components/cockpit-config';
 
 @NgModule({
   imports: [
     // Upgrade module must be the first
     UpgradeModule,
     BrowserAnimationsModule,
-    
-    BsDropdownModule.forRoot(),
-    CollapseModule.forRoot(),
-    BsDatepickerModule.forRoot(),
-    TimepickerModule.forRoot(),
-    
     RouterModule.forRoot(),
     NgRouterModule.forRoot([...UPGRADE_ROUTES], { enableTracing: false, useHash: true }),
     CoreModule.forRoot(),
-    AssetsNavigatorModule,
     ReportsModule,
     NgUpgradeModule,
+    AssetsNavigatorModule,
     DashboardUpgradeModule,
     CockpitDashboardModule,
     SensorPhoneModule,
+    ReportDashboardModule,
     BinaryFileDownloadModule,
-    
-    HttpClientModule,
-    ThingsCloudModule,
-    EmptyModule,
-  ],
-  declarations: [
-  ],
-  entryComponents: [
-  ],
-  providers: [
-    C8yService,
-    C8yCommonService
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
+    SearchModule,
+    SubAssetsModule,
+    ChildDevicesModule,
+    CockpitConfigModule
   ]
 })
 export class AppModule extends HybridAppModule {
--- cockpit/index.ts
+++ cockpit/index.ts
@@ -1,5 +1,6 @@
 import './polyfills';
 import './ng1';
+import './i18n';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -12,5 +13,6 @@ if (__MODE__ === 'production') {
 
 export function bootstrap() {
   return platformBrowserDynamic()
-    .bootstrapModule(AppModule).catch((err) => console.log(err));
+    .bootstrapModule(AppModule)
+    .catch(err => console.log(err));
 }
